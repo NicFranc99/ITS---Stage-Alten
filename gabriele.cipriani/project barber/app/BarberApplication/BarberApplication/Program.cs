96d95d2aa60a952bfb65c84a0eb6cbc5dc9ad709
Michele Gabriele CIPRIANI <michele.cipriani@alten.it>
2025-06-12 12:40:10 +0200

    aggiunta feature user

--- gabriele.cipriani/project barber/app/BarberApplication/BarberApplication/Program.cs
+++ gabriele.cipriani/project barber/app/BarberApplication/BarberApplication/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Supermercato_DB;
+using Supermercato_DB.Interfaces;
+using Supermercato_DB.Repos;
+
+IUserRepository databaseUser = new UserRepository();
+
+User user = new User();
+
+do
+{
+    do
+    {
+        Console.WriteLine("\nInserimento credenziali Utente\n");
+
+    } while (user.InserisciCredenziali() == false);
+
+
+    Console.WriteLine("Ricerca dell' utente nel DB");
+
+} while (databaseUser.GetUser(user) == false);
---
+++ gabriele.cipriani/project barber/app/BarberApplication/BarberApplication/Repos/UserRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Supermercato_DB.Interfaces;
+
+namespace Supermercato_DB.Repos
+{
+    public class UserRepository : IUserRepository
+    {
+        readonly string _connectionstring = @"Server=tcp:its-alen-bari.database.windows.net,1433;Initial Catalog=its-alten-bari;Persist Security Info=False;User ID=nicola.francavilla;Password=2Vm&aic&AMo-#pxL;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;";
+        public bool GetUser(User user)
+        {
+            string query = "SELECT Username,Password FROM Users WHERE username=@username AND password =@password";
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionstring))
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        connection.Open();
+                        command.Parameters.AddWithValue("username", user.Username);
+                        command.Parameters.AddWithValue("password", user.Password);
+
+                        var reader = command.ExecuteReader();
+
+
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"Username :{reader.GetString(0)} Password : {reader.GetString(1)}");
+                        }
+
+                        if (!reader.HasRows)
+                        {
+                            Console.WriteLine("!! Nessun Utente Trovato !!");
+                            return false;
+                        }
+                        return true;
+                    }
+                }
+            }
+            catch (SqlException)
+            {
+                Console.WriteLine("Query non riuscita");
+                return false;
+            }
+        }
+    }
+}
