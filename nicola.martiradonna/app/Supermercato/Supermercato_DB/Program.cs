ba8316cb269b1ceac197ad11a1492c76a1f8cb69
Nicola MARTIRADONNA <nicola.martiradonna@alten.it>
2025-06-12 10:38:12 +0200

    Inserimento credenziali con ricerca nel db

---
+++ nicola.martiradonna/app/Supermercato/Supermercato_DB/Program.cs
@@ -0,0 +1,18 @@
+﻿using Supermercato_DB;
+using Supermercato_DB.Interfaces;
+using Supermercato_DB.Repos;
+
+IUserRepository databaseUser = new UserRepository();
+
+User user = new User();
+
+do
+{
+    Console.WriteLine("\nInserimento credenziali Utente\n");
+
+} while (user.InserisciCredenziali() == false);
+
+do
+{
+    Console.WriteLine("Ricerca dell' utente nel DB");
+} while (databaseUser.GetUser(user) == false);
