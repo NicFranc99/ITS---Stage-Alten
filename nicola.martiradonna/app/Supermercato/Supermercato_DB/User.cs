b24acf499b5ff3a8e0ca37d01b0a962cbb24b0fc
Nicola MARTIRADONNA <nicola.martiradonna@alten.it>
2025-06-12 10:38:12 +0200

    Inserimento credenziali con ricerca nel db

---
+++ nicola.martiradonna/app/Supermercato/Supermercato_DB/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermercato_DB
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+
+        public bool InserisciCredenziali()
+        {
+            
+                Console.WriteLine("Inserisci il tuo Username");
+                Username = Console.ReadLine();
+
+                Console.WriteLine("Inserisci la tua Password");
+                Password = Console.ReadLine();
+
+                
+            if(Username=="" || Password == "")
+            {
+                Console.WriteLine("!!  Errore credenziali  !!");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Credenziali OK");
+                return true;
+            }
+        }
+
+    }
+        
+}
