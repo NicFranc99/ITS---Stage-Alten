59da37b09c51b9ed42245e0cc3edca4a6d049798
Nicola MARTIRADONNA <nicola.martiradonna@alten.it>
2025-06-18 15:12:46 +0200

    Aggiunta metodo con singola query

--- nicola.martiradonna/app/Supermercato/Supermercato_DB/Repos/ProductRepository.cs
+++ nicola.martiradonna/app/Supermercato/Supermercato_DB/Repos/ProductRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Data.SqlClient;
@@ -12,8 +13,6 @@ namespace Supermercato_DB.Repos
     public class ProductRepository : IProductRepository
     {
         readonly string _connectionstring = @"Server=tcp:its-alen-bari.database.windows.net,1433;Initial Catalog=its-alten-bari;Persist Security Info=False;User ID=nicola.francavilla;Password=2Vm&aic&AMo-#pxL;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;";
-
-
 
         public bool GetAllProducts()
         {
@@ -34,7 +33,12 @@ namespace Supermercato_DB.Repos
                         while (reader.Read())
                         {
                             Console.WriteLine("\nProdotto:");
-                            Console.WriteLine($"Id: {reader.GetInt64(0)} Nome: {reader.GetString(1)} Prezzo: {reader.GetDecimal(2)} Descrizione: {reader.GetString(3)} Quantità: {reader.GetInt32(4)}");
+                            Console.WriteLine(
+                                $"Id: {reader.GetInt64(0)} " +
+                                $"Nome: {reader.GetString(1)} " +
+                                $"Prezzo: {reader.GetDecimal(2)} " +
+                                $"Descrizione: {reader.GetString(3)} " +
+                                $"Quantità: {reader.GetInt32(4)}");
                         }
                         if (!reader.HasRows)
                         {
@@ -270,9 +274,7 @@ namespace Supermercato_DB.Repos
 
                                 if (!(prodotto.Nome == null))
                                 {
-
                                     command.Parameters.AddWithValue("nome", prodotto.Nome);
-
                                 }
                                 else
                                 {
@@ -304,6 +306,11 @@ namespace Supermercato_DB.Repos
                                     command.Parameters.AddWithValue("idCategoria", reader.GetInt64(3));
                                 }
                             }
+                            if (!reader.HasRows)
+                            {
+                                Console.WriteLine($"!! Nessun prodotto trovato con id: {idProdotto}  !!");
+                                return false;
+                            }
                             reader.Close();
                         }
 
@@ -316,6 +323,95 @@ namespace Supermercato_DB.Repos
                             Console.WriteLine("!! Prodotto aggiornato con successo  !!");
                             return true;
 
+                        }
+                        Console.WriteLine($"!!  Nessun prodotto trovato con id: {idProdotto}  !!");
+                        return false;
+
+                    }
+                }
+            }
+            catch (SqlException)
+            {
+                Console.WriteLine("!! Query non riuscita  !!");
+                return false;
+
+            }
+        }
+
+        public bool UpdateProductByIdString(Product prodotto, int idProdotto)
+        {
+            string query = "UPDATE market.prodotti SET";
+
+            //SE TUTTE LE PROPRIETA' DEL PRODUCT NON SONO NULL
+            if (!(prodotto.Nome == null) && !(prodotto.Prezzo == 0) && !(prodotto.Quantita == 0) && !(prodotto.Id_Categoria == 0))
+            {
+                query += " Nome=@nome ,Prezzo=@prezzo ,Quantita=@quantita,Id_Categoria = @idCategoria";
+            }
+            //SE SOLO IL PREZZO DEL PRODOTTO NON E' NULL
+            if (!(prodotto.Prezzo == 0) && (prodotto.Quantita ==0) && (prodotto.Nome==null) && (prodotto.Id_Categoria==0))
+            {
+                query += " Prezzo= @prezzo";
+            }
+            // SE SOLO LA QUANTITA' E IL PREZZO NON SONO NULL
+            if (!(prodotto.Quantita == 0) && !(prodotto.Prezzo == 0) && (prodotto.Nome== null) && (prodotto.Id_Categoria == 0))
+            {
+                query += " Prezzo= @prezzo ,Quantita=@quantita";
+            }
+            // SE SOLO L' ID CATEGORIA NON E' NULL
+            if (!(prodotto.Id_Categoria == 0) && (prodotto.Quantita == 0) && (prodotto.Nome == null) && (prodotto.Prezzo == 0))
+            {
+                query += " Id_Categoria= @idCategoria";
+            }
+
+            query += " WHERE Id = @id";
+
+                try
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionstring))
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        
+                        
+                        if (!(prodotto.Nome == null) && !(prodotto.Prezzo == 0) && !(prodotto.Quantita == 0) && !(prodotto.Id_Categoria == 0))
+                        {
+                            
+                            command.Parameters.AddWithValue("nome", prodotto.Nome);
+                            command.Parameters.AddWithValue("prezzo", prodotto.Prezzo);
+                            command.Parameters.AddWithValue("quantita", prodotto.Quantita);
+                            command.Parameters.AddWithValue("idCategoria", prodotto.Id_Categoria);
+                            command.Parameters.AddWithValue("id", idProdotto);
+                        }
+                                  
+                        if (!(prodotto.Prezzo == 0) && (prodotto.Quantita == 0) && (prodotto.Nome == null) && (prodotto.Id_Categoria == 0))
+                        {
+                            
+                            command.Parameters.AddWithValue("prezzo", prodotto.Prezzo);
+                            command.Parameters.AddWithValue("id", idProdotto);
+                        }
+                       
+                        if (!(prodotto.Quantita == 0) && !(prodotto.Prezzo == 0) && (prodotto.Nome == null) && (prodotto.Id_Categoria == 0))
+                        {
+                            
+                            command.Parameters.AddWithValue("prezzo", prodotto.Prezzo);
+                            command.Parameters.AddWithValue("quantita", prodotto.Quantita);
+                            command.Parameters.AddWithValue("id", idProdotto);
+                        }
+                       
+                        if (!(prodotto.Id_Categoria == 0) && (prodotto.Quantita == 0) && (prodotto.Nome == null) && (prodotto.Prezzo == 0))
+                        {
+                            
+                            command.Parameters.AddWithValue("idCategoria", prodotto.Id_Categoria);
+                            command.Parameters.AddWithValue("id", idProdotto);
+                        }
+                        connection.Open();
+                       
+                          int rowsAffected= command.ExecuteNonQuery();
+
+                        if(rowsAffected > 0)
+                        {
+                            Console.WriteLine("!! Prodotto aggiornato con successo  !!");
+                            return true;
                         }
                         Console.WriteLine($"!!  Nessun prodotto trovato con id: {idProdotto}  !!");
                         return false;
